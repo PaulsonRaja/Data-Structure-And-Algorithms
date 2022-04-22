4071a7c283a2feb0ec430a15e92679bbf9432ec1
PaulsonRaja <lpaulsonraja@gmail.com>
2022-04-22 22:20:29 +0530

    Added problem of setting, clearing and toggling particular bits.

---
+++ Bit-Manipulation/clear_kth_bit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void clear_kth_bit(int &num, int pos)
+{
+    num = num & (~(1 << pos));
+}
+
+int main()
+{
+    int num = 0;
+    int pos = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d", &num);
+
+    printf("Enter the bit postion to be set (0 based indexing):\n");
+    scanf("%d", &pos);
+
+    clear_kth_bit(num, pos);
+
+    printf("After clearing the %d bit: %d\n", pos, num);
+    return 0;
+}
