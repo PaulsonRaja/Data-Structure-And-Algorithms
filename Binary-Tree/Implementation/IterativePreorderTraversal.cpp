788ab76e33f938c49dc6173d3332e543c7ba4b6f
PaulsonRaja <lpaulsonraja@gmail.com>
2022-08-31 23:49:20 +0530

    Added problem iterative preorder traversal.

---
+++ Binary-Tree/Implementation/IterativePreorderTraversal.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct node {
+    int val;
+    struct node *left;
+    struct node *right;
+    node(int val);
+};
+
+node::node(int val)
+{
+    this->val = val;
+    left = NULL;
+    right = NULL;
+}
+
+/*
+ * TC - O(N) ( We have traverse all the nodes. )
+ * SC - O(N) ( Height of the tree. )
+ */
+void iterative_preorder_traversal(node *root)
+{
+    stack<struct node *> st;
+    if (root == NULL)
+	return;
+    st.push(root);
+    while (!st.empty()) {
+	struct node *curr_node = st.top();
+	st.pop();
+	cout << curr_node->val << " ";
+	if (curr_node->right != NULL)
+	    st.push(curr_node->right);
+	if (curr_node->left != NULL)
+	    st.push(curr_node->left);
+    }
+}
+
+struct node *create_tree()
+{
+    struct node *root = NULL;
+    int root_data;
+
+    cout << "Enter the node value:" << endl;
+    cin >> root_data;
+    root = new struct node (root_data);
+    if (root_data == -1)
+	return NULL;
+    root->left = create_tree();
+    root->right = create_tree();
+    return root;
+}
+
+void print_tree_detailed(struct node *root)
+{
+    if (root == NULL)
+	return;
+    cout << root->val << ": ";
+    if (root->left != NULL)
+	cout << "L: " << root->left->val << ", ";
+    if (root->right != NULL)
+	cout << "R: " << root->right->val << "";
+    cout << "\n";
+    print_tree_detailed(root->left);
+    print_tree_detailed(root->right);
+}
+
+int main()
+{
+    struct node *root = NULL;
+
+    cout << "Enter the node values use -1 for no left or right child" << endl;
+    root = create_tree();
+    cout << "\n";
+
+    cout << "Tree representation:" << endl;
+    print_tree_detailed(root);
+    cout << "\n";
+
+    cout << "Preorder traversal of tree:" << endl;
+    iterative_preorder_traversal(root);
+    cout << "\n";
+    return 0;
+}
